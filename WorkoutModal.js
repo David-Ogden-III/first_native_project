98e238f903391d474ce2ecd982a2bcbd123d1dce
David Ogden <david-ogden699@hotmail.com>
2023-06-13 18:11:11 -0700

    Added Picker and DateTimePicker to WorkoutModal

--- WorkoutModal.js
+++ WorkoutModal.js
@@ -1,22 +1,32 @@
-import { Text, View, TextInput } from 'react-native';
+import { View } from 'react-native';
 import { Button, Dialog } from '@rneui/themed';
 import { useState } from 'react';
 import { Formik } from 'formik';
-
+import { Picker } from '@react-native-picker/picker';
+import DateTimePicker from '@react-native-community/datetimepicker'
 
 
 const WorkoutModal = ({ onSubmit }) => {
   const [modalOpen, setModalOpen] = useState(false);
+  const [focus, setFocus] = useState('Push')
+  const [date, setDate] = useState(new Date());
 
   const handleSubmit = (values) => {
     const newTitle = {
       focus: values.focus,
-      date: values.date
+      date: date.toLocaleDateString('en-US')
     }
     onSubmit(newTitle);
     setModalOpen(false)
     console.log(newTitle)
+    setFocus('Push')
+    setDate(new Date());
   }
+
+  const onDateChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setDate(currentDate);
+  };
 
   return (
     <View>
@@ -33,32 +43,43 @@ const WorkoutModal = ({ onSubmit }) => {
         <Dialog.Title title='Select Focus and Date' />
         <Formik
           initialValues={{
-            focus: '',
+            focus: 'Push',
             date: ''
           }}
           onSubmit={handleSubmit}
         >
-            {({ handleChange, handleBlur, handleSubmit, values }) => (
-              <View>
-                <TextInput
-                  style={{ borderWidth: 1, padding: 5, margin: 6 }}
-                  onChangeText={handleChange('focus')}
-                  onBlur={handleBlur('focus')}
-                  value={values.focus}
-                  placeholder='Choose a Focus'
-                />
+          {({ setFieldValue, handleSubmit, values }) => (
+            <View>
 
-                <TextInput
-                  style={{ borderWidth: 1, padding: 5, marginHorizontal: 6, marginBottom: 6 }}
-                  onChangeText={handleChange('date')}
-                  onBlur={handleBlur('date')}
-                  value={values.date}
-                  placeholder='Choose a Date'
-                  type='date'
+              <View>
+                <Picker
+                  value={values.focus}
+                  selectedValue={focus}
+                  onValueChange={(itemValue) => {
+                    setFieldValue('focus', itemValue)
+                    setFocus(itemValue)
+                  }}
+                >
+                  <Picker.Item label='Push' value='Push' />
+                  <Picker.Item label='Pull' value='Pull' />
+                  <Picker.Item label='Legs' value='Legs' />
+                </Picker>
+              </View>
+
+              <View style={{ alignItems: 'center'}}>
+                <DateTimePicker
+                  value={date}
+                  mode='date'
+                  display='default'
+                  onChange={onDateChange}
                 />
+              </View>
+
+              <View>
                 <Button onPress={handleSubmit} title="Submit" />
               </View>
-            )}
+            </View>
+          )}
         </Formik>
       </Dialog>
     </View>
